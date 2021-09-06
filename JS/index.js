2fc70797c9e053ecd540a59366b2c4efff6eeaf7
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-09-06 23:47:02 +0100

    Implementing vue.js

--- JS/index.js
+++ JS/index.js
@@ -13,10 +13,20 @@ const app = new Vue({
                 taskText: 'Comer Funge',
                 isCompleted: true
             }
-        ]
+        ],
+        quantItems: 0
     },
     methods: {
-
+        changeTab: function (e) {
+            this.activeTab = e.target.innerText.toLowerCase();
+        },
+        switchTaskStatus: function (taskId) {
+            for (let i = 0; i < this.taskList.length; i++) {
+                if (this.taskList[i].taskId == taskId) {
+                    this.taskList[i].isCompleted = !this.taskList[i].isCompleted;
+                }
+            }
+        }
     },
     components: {
 
