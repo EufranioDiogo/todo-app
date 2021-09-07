be9c8f8e394acd9f7a1aa798dd61b2a46c1f7e32
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-09-07 11:16:16 +0100

    Theme Switcher done

--- JS/index.js
+++ JS/index.js
@@ -1,32 +1,76 @@
-let element = 0;
+
+
+function getActualTheme() {
+    const actualTheme = localStorage.getItem('theme');
+
+    if (actualTheme == null || actualTheme == 'undefined') {
+        setDarkTheme();
+    } else {
+        if (actualTheme === 'dark') {
+            setDarkTheme();
+        } else {
+            setWhiteTheme();
+        }
+    }
+
+    return actualTheme;
+}
+
+function setWhiteTheme() {
+    localStorage.setItem('theme', 'white');
+
+    document.querySelector('.theme-file-src').href = 'CSS/light-theme.css';
+
+    document.querySelector('.theme-toggle-button--icon').src = '../IMAGES/icon-moon.svg';
+}
+
+function setDarkTheme() {
+    localStorage.setItem('theme', 'dark');
+
+    document.querySelector('.theme-file-src').href = 'CSS/dark-theme.css';
+    document.querySelector('.theme-toggle-button--icon').src = '../IMAGES/icon-sun.svg';
+}
+
+
+
+
+
 const app = new Vue({
     el: '#app',
     data: {
         activeTab: 'all',
         taskList: [
         ],
-        quantItems: 0,
         totalTasks: 0,
-        newTask: ''
+        newTask: '',
+        quantCompleted: 0,
+        quantActive: 0,
+        isDarkTheme: true
     },
     methods: {
         changeTab: function (e) {
             this.activeTab = e.target.innerText.toLowerCase();
         },
         switchTaskStatus: function (event, taskId) {
-            event.stopPropagation();
-            event.preventDefault();
-
             let i;
             for (i = 0; i < this.taskList.length; i++) {
                 if (this.taskList[i].taskId == taskId) {
                     this.taskList[i].isCompleted = !this.taskList[i].isCompleted;
+
+                    if(this.taskList[i].isCompleted) {
+                        this.quantCompleted++;
+                        this.quantActive--;
+                    } else {
+                        this.quantCompleted--;
+                        this.quantActive++;
+                    }
                     break;
                 }
             }
         },
         clearCompleted: function () {
-            this.taskList.filter((task) => !task.isCompleted);
+            this.taskList = this.taskList.filter((task) => !task.isCompleted);
+            this.quantCompleted = 0;
         },
         addNewTask: function () {
             if (this.newTask != '') {
@@ -35,10 +79,43 @@ const app = new Vue({
                     taskText: this.newTask,
                     isCompleted: false
                 })
+                this.quantActive++;
             }
-        }
+            this.newTask = '';
+        },
+        deleteTask: function(taskId) {
+            this.taskList = this.taskList.filter((task) => {
+                if (task.taskId != taskId) {
+                    return true;
+                }
+                if (task.isCompleted) {
+                    this.quantCompleted--;
+                } else {
+                    this.quantActive--;
+                }
+                return false;
+            })
+        },
+        changeTheme: function () {
+            this.isDarkTheme = getActualTheme() == 'dark' ? true : false;
+
+            if (this.isDarkTheme) {
+                setWhiteTheme();
+            } else {
+                setDarkTheme();
+            }
+            this.isDarkTheme = !this.isDarkTheme;
+        },
+        
     },
     components: {
 
+    },
+    computed: {
+        quantItems: function() {
+            return this.quantCompleted + this.quantActive;
+        }
     }
 })
+
+getActualTheme();
